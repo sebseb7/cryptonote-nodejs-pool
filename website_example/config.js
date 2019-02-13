4a50a4fe62a3d72fee295e8a1b4f137e2729482b
sebseb7 <sebgreenbus@gmail.com>
2019-02-13 22:55:25 +0100

    website updates for swap2

--- website_example/config.js
+++ website_example/config.js
@@ -6,8 +6,8 @@ var discord = "https://discordapp.com/invite/YourPool";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var blockchainExplorer = "https://swap.coinscope.cc/block/{id}";
+var transactionExplorer = "https://swap.coinscope.cc/tx/{id}";
 
 var themeCss = "themes/default.css";
 var defaultLang = 'en';
